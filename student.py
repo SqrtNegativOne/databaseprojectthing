48059a4fb9adb56ef98f58a9ef8f37a1572d4486
Karan Gupta <80627097+KaranGupta06@users.noreply.github.com>
2023-05-10 16:57:18 +0530

    Update student.py

--- student.py
+++ student.py
@@ -5,4 +5,8 @@ STUDENT_FILE_PATH = ''
 fields = ['Name', 'Admission number', 'Subjects']
 
 def signup():
-  with open(STUDENT_FILE_PATH, 'a') as f:
+    with open(STUDENT_FILE_PATH, 'a') as f:
+        ...
+
+def login():
+    ...
