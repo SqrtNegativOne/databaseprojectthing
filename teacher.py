6b2d0aca428e4faed8a16762371f5f21d7b2fad2
Karan Gupta <80627097+KaranGupta06@users.noreply.github.com>
2023-05-10 17:19:13 +0530

    Update teacher.py

--- teacher.py
+++ teacher.py
@@ -1,1 +1,32 @@
+from collections import namedtuple
+import pickle
 
+FILE_PATH = ...
+
+teacher = namedtuple("teacher", ["name", "adm_no", "subject", "password"])
+
+def signup(user):
+    with open(FILE_PATH, "wb") as f:
+        pickle.dump(user, f)
+
+def signin(password) -> bool:
+    with open(FILE_PATH, "rb") as f:
+        while True:
+            try:
+                user = pickle.load(f)
+            except EOFError:
+                return False
+            if user["password"] == password:
+                return user
+
+def edit_user_data(user) -> bool:
+    with open(FILE_PATH, "rb") as f:
+        while True:
+            try:
+                data = pickle.load(f)
+            except EOFError:
+                return False
+            if data == user:
+                break
+        
+    with open(File)
