1ce99f9d73a02c4928ac9f37118f01d8ee7bfd4e
SqrtNegativOne <121613377+SqrtNegativOne@users.noreply.github.com>
2023-05-14 09:18:41 +0530

    Update student.py
    ye lo karan

--- student.py
+++ student.py
@@ -1,12 +1,26 @@
 import csv
 
-STUDENT_FILE_PATH = ''
-
-fields = ['Name', 'Admission number', 'Subjects']
-
-def signup():
-    with open(STUDENT_FILE_PATH, 'a') as f:
-        ...
-
-def login():
-    ...
+databaseprojectthing = """
+░█▀▄░█▀█░▀█▀░█▀█░█▀▄░█▀█░█▀▀░█▀▀░░█▀█░█▀▄░█▀█░▀▀█░█▀▀░█▀▀░▀█▀░░▀█▀░█░█░▀█▀░█▀█░█▀▀
+░█░█░█▀█░░█░░█▀█░█▀▄░█▀█░▀▀█░█▀▀░░█▀▀░█▀▄░█░█░░░█░█▀▀░█░░░░█░░░░█░░█▀█░░█░░█░█░█░█
+░▀▀░░▀░▀░░▀░░▀░▀░▀▀░░▀░▀░▀▀▀░▀▀▀░░▀░░░▀░▀░▀▀▀░▀▀░░▀▀▀░▀▀▀░░▀░░░░▀░░▀░▀░▀▀▀░▀░▀░▀▀▀
+                                                                                                                                                                          
+"""
+print(databaseprojectthing)
+print('Welcome to the student login page.')
+with open('students.csv', 'r+') as databasething:
+    studentList = list(csv.reader(databasething))
+    print(studentList)
+while True:
+    name = input('Enter your name: ') # I would have used a walrus operator, but the computer lab only has python 3.7, not the required python 3.8 :harold:
+    if name not in [row[0] for row in studentList]:
+        print('Error: name was not found in student database. Kindly contact the school administrator.')
+    else:
+        admno = input('Enter your admission number: ')
+        if admno not in [row[1] for row in studentList]:
+            print('Error: wrong admission number. Make sure your name wasn\'t mispelled.')
+        else:
+            # Now it begins.
+            pass
+    if input('Continue? (y/n): ') == 'n':
+        break
