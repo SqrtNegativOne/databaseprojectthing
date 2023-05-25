ccbd35dba2c176f6872fd112950653e963664a26
Karan Gupta <80627097+KaranGupta06@users.noreply.github.com>
2023-05-25 22:55:01 +0530

    Update TkinterUI.py

--- TkinterUI.py
+++ TkinterUI.py
@@ -1,34 +1,100 @@
-#misc
+import teacher_backend as teacher
+import student_backend as student
 import tkinter as tk
 
-root = tk.Tk()
+databaseprojectthing = """
+█▀▄░█▀█░▀█▀░█▀█░█▀▄░█▀█░█▀▀░█▀▀░█▀█░█▀▄░█▀█░▀▀█░█▀▀░█▀▀░▀█▀░▀█▀░█░█░▀█▀░█▀█░█▀▀
+█░█░█▀█░░█░░█▀█░█▀▄░█▀█░▀▀█░█▀▀░█▀▀░█▀▄░█░█░░░█░█▀▀░█░░░░█░░░█░░█▀█░░█░░█░█░█░█
+▀▀░░▀░▀░░▀░░▀░▀░▀▀░░▀░▀░▀▀▀░▀▀▀░▀░░░▀░▀░▀▀▀░▀▀░░▀▀▀░▀▀▀░░▀░░░▀░░▀░▀░▀▀▀░▀░▀░▀▀▀
+"""
 
-root.state("zoomed")
-root.title("School Portal")
+class App(tk.Tk):
 
-top_frm = tk.Frame(root, bg="red")
-dwn_frm = tk.Frame(root, bg="green")
-lft_frm = tk.Frame(root, bg="yellow")
-rgt_frm = tk.Frame(root, bg="orange")
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
 
-top_frm.pack(side="top", fill="x")
-dwn_frm.pack(side="bottom", fill="x")
-lft_frm.pack(side="left", fill="y")
-rgt_frm.pack(side="right", fill="y")
+        self.state("zoomed")
+        self.title("Database Project thing")
 
-tk.Button(top_frm, height=2, width=2).pack(anchor="center")
-tk.Button(dwn_frm, height=2, width=2).pack(anchor="center")
-tk.Button(lft_frm, height=2, width=2).pack(anchor="center")
-tk.Button(rgt_frm, height=2, width=2).pack(anchor="center")
+        self.current_widgets = []
+
+        self.top_frm = tk.Frame(self)
+        self.dwn_frm = tk.Frame(self)
+        self.lft_frm = tk.Frame(self)
+        self.rgt_frm = tk.Frame(self)
+        self.mid_frm = tk.Frame(self)
+
+        self.top_frm.pack(side="top", fill="x")
+        self.dwn_frm.pack(side="bottom", fill="x")
+        self.lft_frm.pack(side="left", fill="y")
+        self.rgt_frm.pack(side="right", fill="y")
+        self.mid_frm.pack(anchor="center", fill="both", expand=1)
+
+        # tk.Button(dwn_frm, height=5, width=5).pack(anchor="center")
+        # tk.Button(lft_frm, height=5, width=5).pack(anchor="center")
+        # tk.Button(rgt_frm, height=5, width=5).pack(anchor="center")
+
+        title_label = tk.Label(self.top_frm, text=databaseprojectthing, font="helvatica 15 bold")
+        title_label.pack(anchor="center", pady=10)
+
+        self.title_screen()
 
 
-def title_screen() -> list:
-    ...
+    def widget_clearer(self, func):
+        def in_func():
+            for i in self.current_widgets:
+                i.destroy()
 
-def student_screen():
-    ...
+            widgets = func()
 
-def teacher_screen():
-    ...
+            self.current_widgets.clear
+            self.current_widgets += widgets
 
-root.mainloop()
+        return in_func()
+
+    @widget_clearer
+    def signin(self, username, password, typ):
+
+        if typ == "Teacher":
+            ...
+        else:
+            ...
+
+        self.refresh_widgets()
+
+    @widget_clearer
+    def signup(self):
+        ...
+
+    @widget_clearer
+    def title_screen(self) -> None:
+        box_frm = tk.Frame(self.mid_frm)
+        box_frm.pack(anchor="center")
+
+        member_type = tk.StringVar(value="Teacher")
+
+        lbl1 = tk.Label(box_frm, text="Username: ")
+        lbl2 = tk.Label(box_frm, text="Password: ")
+        ety1 = tk.Entry(box_frm)
+        ety2 = tk.Entry(box_frm)
+        menu = tk.OptionMenu(box_frm, member_type, "Teacher", "Student")
+
+        ok_btn = tk.Button(box_frm, text="OK",
+                           command=lambda: self.signin(ety1.get(), ety2.get(), member_type.get()))
+        su_btn = tk.Button(box_frm, text="Signup",
+                           command=self.signup)
+
+        lbl1.grid(row=0, column=0)
+        lbl2.grid(row=1, column=0)
+        ety1.grid(row=0, column=1)
+        ety2.grid(row=1, column=1)
+        menu.grid(row=2, column=1)
+        ok_btn.grid(row=3, column=0)
+        su_btn.grid(row=3, column=1)
+
+        return box_frm
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
