ee5f1c086f8a4426136abc29a1506095a550b242
Karan Gupta <80627097+KaranGupta06@users.noreply.github.com>
2023-05-25 11:50:51 +0530

    Update teacher.py
    ye le ark

--- teacher.py
+++ teacher.py
@@ -1,31 +1,45 @@
 from collections import namedtuple
-import hashlib
+import hashlib, pickle
 import pickle
+import os
+
+databaseprojectthing = """
+░█▀▄░█▀█░▀█▀░█▀█░█▀▄░█▀█░█▀▀░█▀▀░░█▀█░█▀▄░█▀█░▀▀█░█▀▀░█▀▀░▀█▀░░▀█▀░█░█░▀█▀░█▀█░█▀▀
+░█░█░█▀█░░█░░█▀█░█▀▄░█▀█░▀▀█░█▀▀░░█▀▀░█▀▄░█░█░░░█░█▀▀░█░░░░█░░░░█░░█▀█░░█░░█░█░█░█
+░▀▀░░▀░▀░░▀░░▀░▀░▀▀░░▀░▀░▀▀▀░▀▀▀░░▀░░░▀░▀░▀▀▀░▀▀░░▀▀▀░▀▀▀░░▀░░░░▀░░▀░▀░▀▀▀░▀░▀░▀▀▀
+"""
 
 FILE_PATH = "teacher_data.txt"
 HW_FILE_PATH = "homework.txt"
 
 teacher = namedtuple("teacher", ["name", "emp_no", "subject", "password"])
 
-def signup(user):
+
+def signup(user) -> teacher:
    
-    emp_no = hashlib.sha1(user["name"].encode("UTF-8")).hexdigest()[:5]
-    user = user._replace(emp_no=emp_no)
+    N_empnum = hashlib.sha1(user.name.encode("UTF-8")).hexdigest()[:5]
+    N_passwd = hashlib.sha1(user.password.encode("UTF-8"), usedforsecurity=True).hexdigest()
+
+    user = user._replace(emp_no=N_empnum, password=N_passwd)
 
     with open(FILE_PATH, "ab") as f:
         pickle.dump(user, f)
+    
+    return user
 
-def signin(emp_no, password) -> bool:
+def signin(emp_no, password) -> teacher|bool:
+    password = hashlib.sha1(password.encode("UTF-8"), usedforsecurity=True).hexdigest()
+
     with open(FILE_PATH, "rb") as f:
         while True:
             try:
                 user = pickle.load(f)
             except EOFError:
                 return False
-            if user["password"] == password and user["emp_no"] == user["emp_no"]:
+            if user.password == password and emp_no == user.emp_no:
                 return user
 
-def edit_user_data(user, attrs) -> bool:
+def edit_user_data(user, attrs) -> None:
 
     data = []
 
@@ -42,36 +56,67 @@ def edit_user_data(user, attrs) -> bool:
                 pickle.dump(user._replace(**attrs), f)
             else:
                 pickle.dump(i, f)
+    
+    return user._replace(**attrs)
 
 def assign_hw(user, hw_data) -> None:
 
-    with open(HW_FILE_PATH, "a") as f:
-       
+    with open(HW_FILE_PATH, "a") as f:   
         f.write(user.subject.upper().ljust(10) + hw_data)
-   
 
-# signup(teacher("karan", 7439, "phy", "cat"))
-# signup(teacher("acide", 7436, "chm", "dog"))
-# signup(teacher("topper", 1000, "PE", "PCC"))
 
-# with open(FILE_PATH, "r+b") as f:
-#     while True:
-#         try:
-#             print(pickle.load(f))
-#         except EOFError:
-#             break
+def print_banner(subtext=None) -> None:
+    os.system("cls")
+    print(databaseprojectthing)
+    if subtext:
+        print(subtext)
+    print()
 
-# edit_user_data(teacher("karan", 7439, "phy", "cat"), {"name":"koooo"})
-# print("-"*10)
+user = None
+subtext = "Welcome!"
 
-# with open(FILE_PATH, "r+b") as f:
-#     while True:
-#         try:
-#             print(pickle.load(f))
-#         except EOFError:
-#             break
+while not user:
+    print_banner(subtext)
+    login_state = input("Signin/Signup/Exit: ").casefold()
 
-# assign_hw(teacher("koooo", 7439, "phy", "cat"), "DO UR WORK NOWWWW!")
+    if login_state == "signin":
+        username = input("Employee Number: ")
+        password = input("Password: ")
+        user = signin(username, password)
 
-# with open(HW_FILE_PATH, "r") as f:
-#     print(f.read())
+    elif login_state == "signup":
+        name     = input("Name: ")
+        subject  = input("Subject: ")
+        password = input("Password: ")
+        user = signup(teacher(name, None, subject, password))
+    
+    elif login_state == "exit":
+        exit()
+
+    else:
+        input("\n\u26A0 Invalid prompt.\nPress Enter to continue.")
+
+
+while True:
+    print_banner(f"Signed in as: \t\t{user.name.capitalize()}#{user.emp_no}")
+
+    print("Commands:\n| Edit Personal Data (1)| Assign Homework (2) | Exit (3) |")
+    command = input(">>>").casefold()
+
+    if command == "1": #edit personal data
+        field = input("Field name: ")
+        value = input("New Value: ")
+
+        try: 
+            user = edit_user_data(user, {field: value})
+        except ValueError:
+            input("\n\u26A0 Invalid prompt.\nPress Enter to continue.")
+
+    elif command == "2": #assign homework
+        ...
+    
+    elif command == "3": #exit
+        exit()
+    
+    else:
+        input("\n\u26A0 Invalid prompt.\nPress Enter to continue.")
