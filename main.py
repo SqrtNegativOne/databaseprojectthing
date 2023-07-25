ee27780f203e699d45f79a553db3973a9ec408c4
Karan Gupta <80627097+KaranGupta06@users.noreply.github.com>
2023-07-26 00:30:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ import student_backend as student
 
 import tkinter.messagebox as mssg
 import tkinter.ttk as ttk
+import tkcalendar as tkc
 import tkinter as tk
 
-from datetime import datetime
+from datetime import datetime, date
 from random import randint
 from textwrap import wrap
+from math import erf
 
 
-teacher.FILE_PATH    = r"School Project\teacher_data.txt"
-student.FILE_PATH    = r"School Project\students.csv"
-teacher.HW_FILE_PATH = r"School Project\homework.txt"
-student.HW_FILE_PATH = r"School Project\homework.txt"
+teacher.FILE_PATH    = r"...\teacher_data.txt"
+student.FILE_PATH    = r"...\students.csv"
+teacher.HW_FILE_PATH = r"...\homework.txt"
+student.HW_FILE_PATH = r"...\homework.txt"
 
 SUBJECTS = ["English", "Mathematics", "Chemistry", "Physics",
             "German", "Computer Science", "Physical Education"]
@@ -22,6 +24,7 @@ FONT1 = ["Century Gothic", 15]
 FONT2 = ["Century Gothic", 15, "underline"]
 FONT3 = ["Century Gothic", 15, "bold"]
 FONT4 = ["Century Gothic", 10]
+FONT5 = ["Century Gothic", 20]
 
 _student = teacher.namedtuple("_student", ["name", "id", "subject", "password"])
 
@@ -254,16 +257,66 @@ class Sidebar(tk.Frame):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-        self.Vtext = tk.StringVar(self)
+        self.sidebar = tk.Frame(self)
+        self.sidebar.pack_propagate(0)
         
-        banner = tk.Label(self, textvariable=self.Vtext, justify="center", font=FONT1)
-        banner.grid(row=0, column=0, sticky="nsew", pady=2)
+        self.tglbtn = tk.Button(self, text="\u2630", relief="sunken", borderwidth=0,
+                                font=FONT5, command=self.toggle_sidebar)
+        self.prfbtn = tk.Button(self, text="\U0001F464", relief="flat", font=FONT5,
+                                command=lambda: self.event_generate("<<Profile>>"))
 
-        for i, text in enumerate(["Home", "Profile", "Homework", "Exit"]):
+        self.banner = tk.Label(self.sidebar, justify="center", font=FONT1)
+
+        self.togglevar = True
+        self.buttons = []
+
+        for text, sym in zip(["Home", "Profile", "Homework", "Exit"],
+                             ["\U0001F3E0", "\U0001F464", "\U0001F4D6", ""]):
             
-            tk.Button(self, bg="#2C8CBE", fg="white", relief="flat", font=FONT1, text=text,
-                      command=lambda t=text: self.event_generate(f"<<{t}>>", when="now")
-                      ).grid(row=i+1, column=0, sticky="nsew", pady=2)
+            btn = tk.Button(self.sidebar, bg="#2C8CBE", fg="white", relief="sunken", font=FONT5, text=sym,
+                      borderwidth=0, command=lambda t=text: self.event_generate(f"<<{t}>>", when="now"))
+            btn.pack(side="top", fill="x", pady=2)
+
+            self.buttons.append((btn, text, sym))
+        
+        self.buttons[3][0].pack_forget()
+
+        self.tglbtn.pack(side="top", anchor="w")
+        self.sidebar.pack(fill="both", expand=True)
+
+
+    def transition(self, max, target, reverse, n=1):
+        if n==20:
+            self.sidebar["width"] = target
+            return
+        self.sidebar["width"] = max*((1-erf(n/5)) if reverse else erf(n/5))
+        self.after(20, lambda: self.transition(max, target, reverse, n+1))
+
+
+    def toggle_sidebar(self):
+
+        self.after_cancel(self.transition)
+
+        if not self.togglevar:
+            self.transition(150, 40, True)
+            
+            for button in self.buttons:
+                button[0].configure(text=button[2], font=FONT5)
+            
+            self.buttons[3][0].pack_forget()
+            self.banner.pack_forget()
+            self.prfbtn.pack_forget()
+        else:
+            self.transition(150, 150, False)
+
+            for button in self.buttons:
+                button[0].configure(text=button[1], font=FONT1)
+
+            self.buttons[3][0].pack(side="top", fill="x", pady=2)
+            self.banner.pack(side="bottom")
+            self.prfbtn.pack(side="bottom")
+
+        self.togglevar = not self.togglevar
 
 
 class UserProfile(tk.Frame):
@@ -458,7 +511,7 @@ class HomeworkFrame(tk.Frame):
 
         def exit_btn():
             teacher.assign_hw(self.user, ety1.get("1.0", "end").replace("\n", " "),
-                              f"{V_d.get()}/{V_m.get()}/{V_y.get()}")
+                              cal.get_date().strftime(r"%d/%m/%Y"))
             self.main_screen()
             win.destroy()
 
@@ -466,27 +519,21 @@ class HomeworkFrame(tk.Frame):
 
 
         lbl1 = tk.Label(win, font=FONT4, text="Homwork: ")
-        lbl2 = tk.Label(win, font=FONT4, text="Deadline (D/M/Y): ")
+        lbl2 = tk.Label(win, font=FONT4, text="Deadline: ")
 
-        ety1 = tk.Text(win, font=FONT4)
+        ety1 = tk.Text(win, font=FONT4, width=30, height=5)
         btn1 = tk.Button(win, text="ok", font=FONT4, command=exit_btn)
 
-        date = datetime.now()
+        today = date.today()
 
-        V_d = tk.IntVar(win, value=date.day)
-        V_m = tk.IntVar(win, value=date.month)
-        V_y = tk.IntVar(win, value=date.year)
-        
-        timeD = ttk.Spinbox(win, width=5, textvariable=V_d, from_=date.day,   to=31)
-        timeM = ttk.Spinbox(win, width=5, textvariable=V_m, from_=date.month, to=12)
-        timeY = ttk.Spinbox(win, width=5, textvariable=V_y, from_=date.year,  to=2050)
+        cal = tkc.DateEntry(win, year=today.year, month=today.month, day=today.day, 
+                           mindate=date.today(), selectmode="day")
+
         
         lbl1.grid(row=0, column=0, sticky="e")
         ety1.grid(row=0, column=1, columnspan=3, sticky="nsew")
         lbl2.grid(row=1, column=0, sticky="e")
-        timeD.grid(row=1, column=1, padx=2)
-        timeM.grid(row=1, column=2, padx=2)
-        timeY.grid(row=1, column=3, padx=2)
+        cal.grid(row=1, column=1, pady=10)
 
         btn1.grid(row=2, column=3)
         win.mainloop()
@@ -506,6 +553,10 @@ class App(tk.Tk):
         self.signin_frm = Signin(self)
         self.search_frm = SearchBar(self)
         self.sidebar_frm = Sidebar(self)
+        self.middle_frm = tk.Frame(self, height=350, width=350)
+
+        title = tk.Label(self, text="Database Project Thing", font=("Bahnschrift light", 40))
+        title.pack(side="top", fill="x", pady=10)
 
         self.bind("<<UserSignin>>", lambda _: self.signin_portal())
         self.bind("<<UserProfile>>", lambda _: self.switcher("Profile1"))
@@ -514,9 +565,6 @@ class App(tk.Tk):
         for text in ["Home", "Profile", "Homework", "Exit"]:
             self.bind(f"<<{text}>>", lambda _, t=text: self.switcher(t))
 
-        title = tk.Label(self, text="Database Project Thing", font=("Bahnschrift light", 40))
-        title.pack(side="top", fill="x", pady=10)
-        
         self.signin_frm.pack()
         self.signin_frm.signin()
 
@@ -528,17 +576,17 @@ class App(tk.Tk):
 
         self.search_frm.pack(side="right", fill="y")
         self.sidebar_frm.pack(side="left", fill="y", anchor="n")
+        self.middle_frm.place(x=200, y=100)
         
-        frm = Homepage(self)
+        frm = Homepage(self.middle_frm)
         frm.pack(fill="both", anchor="center")
-        self.current_frm = frm
 
         if isinstance(self.user, teacher.teacher):
-            ...
+            pass
         else:
             self.user = _student(self.user[0], self.user[2], self.user[3].split(","), self.user[1])
 
-        self.sidebar_frm.Vtext.set(f"{self.shortname()}#{self.user.id}")
+        self.sidebar_frm.banner["text"] = f"{self.shortname()}\n#{self.user.id}"
     
 
     def shortname(self) -> str:
@@ -551,14 +599,13 @@ class App(tk.Tk):
         return full_name
     
     def post_edit(self):
-        self.user = self.current_frm.user
-        self.sidebar_frm.Vtext.set(f"{self.shortname()}#{self.user.id}")
+        self.user = self.middle_frm.winfo_children()[0].user
+        self.sidebar_frm.banner["text"] = f"{self.shortname()}\n#{self.user.id}"
 
     
     def switch_frm(self, frm):
-        self.current_frm.destroy()
-        self.current_frm = frm
-        self.current_frm.pack(anchor="center", pady=50)
+        self.middle_frm.winfo_children()[0].destroy()
+        frm.pack(anchor="center", fill="y", pady=(50, 0))
 
 
     def switcher(self, event):
@@ -566,13 +613,14 @@ class App(tk.Tk):
         if event == "Exit":
             self.destroy()
         elif event == "Home":
-            self.switch_frm(Homepage(self))
+            self.switch_frm(Homepage(self.middle_frm))
         elif event == "Profile1":
-            self.switch_frm(UserProfile(self, self.search_frm.user, self.user == self.search_frm.user))
+            self.switch_frm(UserProfile(self.middle_frm, self.search_frm.user,
+                                        self.user == self.search_frm.user))
         elif event == "Profile":
-            self.switch_frm(UserProfile(self, self.user, True))
+            self.switch_frm(UserProfile(self.middle_frm, self.user, True))
         elif event == "Homework":
-            self.switch_frm(HomeworkFrame(self, self.user))
+            self.switch_frm(HomeworkFrame(self.middle_frm, self.user))
 
 
 if __name__ == "__main__":
